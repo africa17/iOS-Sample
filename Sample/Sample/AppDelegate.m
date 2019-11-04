ebbff8cc8bcf8a7bd79819a4e11d2e0d7e2f2004
KH RYU <kanghyun.ryu@gmail.com>
2019-11-04 12:33:22 +0900

    Downgrade the target.

--- Sample/Sample/AppDelegate.m
+++ Sample/Sample/AppDelegate.m
@@ -21,7 +21,7 @@
 }
 
 
-#pragma mark - UISceneSession lifecycle
+/*#pragma mark - UISceneSession lifecycle
 
 
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
@@ -36,6 +36,6 @@
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
-
+*/
 
 @end
