37c4f2431c94bf1f867de2d92d52ba62c4ad4042
KH RYU <kanghyun.ryu@gmail.com>
2019-11-04 12:41:09 +0900

    Removed a test case.

--- Sample/SampleUITests/SampleUITests.m
+++ Sample/SampleUITests/SampleUITests.m
@@ -37,12 +37,12 @@
 }
 
 - (void)testLaunchPerformance {
-    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
+    /*if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
         // This measures how long it takes to launch your application.
         [self measureWithMetrics:@[XCTOSSignpostMetric.applicationLaunchMetric] block:^{
             [[[XCUIApplication alloc] init] launch];
         }];
-    }
+    }*/
 }
 
 @end
