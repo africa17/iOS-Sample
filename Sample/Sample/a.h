b70ab5ba62b546cfbbb4b978857d52b48afec7fc
kanghyun.ryu <kanghyun.ryu@lotte.net>
2020-01-23 11:09:38 +0900

    2

---
+++ Sample/Sample/a.h
@@ -0,0 +1,17 @@
+//
+//  a.h
+//  Sample
+//
+//  Created by africa on 23/01/2020.
+//  Copyright © 2020 africa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface a : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sample/Sample/a.m
@@ -0,0 +1,13 @@
+//
+//  a.m
+//  Sample
+//
+//  Created by africa on 23/01/2020.
+//  Copyright © 2020 africa. All rights reserved.
+//
+
+#import "a.h"
+
+@implementation a
+
+@end
